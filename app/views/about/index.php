8621fe369d5460a7732e32bc4fe26ee6644b0a0f
raihannandito <raihannandito@gmail.com>
2024-08-22 11:35:31 +0700

    repo phpmvc

---
+++ app/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-4">
+            <div class="jumbotron">
+                <h1 class="display-4">About Me!</h1>
+                <img src="<?= BASEURL; ?>/img/raihan.jpg" alt="Raihan Nandito Putra" width="100" class="shadow">
+                <hr class="my-4">
+                <p>Halo nama saya <?= $data["nama"]; ?>, umur saya <?= $data["umur"]; ?> tahun, saya adalah seorang <?= $data["pekerjaan"]; ?>.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
+</div>
---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+    <div class="container mt-4">
+            <div class="jumbotron">
+                <h1 class="display-4">Hello, selamat datang di website saya!</h1>
+                <p class="lead">Halo, nama saya <?= $data["nama"]; ?></p>
+                <hr class="my-4">
+                <p>Website ini adalah website sederhana yang saya kembangkan untuk proses pembelajaran saya terkait dengan MVC pada PHP.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="<?= BASEURL; ?>/mahasiswa" role="button">Mahasiswa</a>
+                </p>
+            </div>
+    </div>
